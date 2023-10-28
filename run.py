b368fec119276301dd702745a5a4aefc2a4b160c
Kura <kura.hax@gmail.com>
2023-10-28 15:04:39 +0100

    Add while loop, sleep and minimum of 5 seconds between loop executions

--- run.py
+++ run.py
@@ -4,6 +4,7 @@ import ssl
 from collections import namedtuple
 from os import getenv
 from sys import exit
+from time import monotonic, sleep
 from urllib.request import urlopen
 
 
@@ -56,12 +57,20 @@ except TypeError:
     logger.error("Invalid MONITORS format. Allowed format is \"[name|url|push_token][,name|url|push_token,...]\"")
     exit(1)
 
+logger.info("Loaded the following monitors:")
 for item in items:
-    try:
-        req = urlopen(item.url, timeout=5, context=ssl._create_unverified_context())
-        if req.status >= 200 and req.status <=300:
-            logger.debug(f"'{item.name}' with URL '{item.url}' OK. Sending a ping")
-            urlopen(f"""{push_url.strip("/")}/{item.token}?status=up&msg=OK""", timeout=5)
-    except Exception as e:
-        logger.error(f"'{item.name}' with URL '{item.url}' error. Not sending a ping", exc_info=True)
-        pass
+    logger.info(f"Name: {item.name}, Url: {item.url}, Token: {item.token}")
+
+while True:
+    start = monotonic()
+    for item in items:
+        try:
+            req = urlopen(item.url, timeout=5, context=ssl._create_unverified_context())
+            if req.status >= 200 and req.status <=300:
+                logger.debug(f"'{item.name}' with URL '{item.url}' OK. Sending a ping")
+                urlopen(f"""{push_url.strip("/")}/{item.token}?status=up&msg=OK""", timeout=5)
+        except Exception as e:
+            logger.error(f"'{item.name}' with URL '{item.url}' error. Not sending a ping", exc_info=True)
+            pass
+    end = monotonic()
+    sleep(max(10, 60 - (end - start)))
